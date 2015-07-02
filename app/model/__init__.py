253f0d48b9f60fd316a6e3f4ce73d7a0d4ba1967
unknown <tanghaomo@126.com>
2015-07-03 00:00:37 +0800

    Test in local

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -2,8 +2,12 @@ from structure import *
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+import os
 
-engine = create_engine('mysql://tang:a1a2a3a4@localhost/healthcare')
+if os.getenv("COMPUTERNAME") == "ALSTON-TABLET":
+    engine = create_engine('mysql://root@127.0.0.1/healthcare')
+else:
+    engine = create_engine('mysql://tang:a1a2a3a4@localhost/healthcare')
 #Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
