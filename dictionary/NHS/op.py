82967e34ca360e9912af361e5412e221040cecb2
Alston-Tang <tanghaomo@126.com>
2015-07-23 18:21:53 +0800

    Copy NHS data

--- dictionary/NHS/op.py
+++ dictionary/NHS/op.py
@@ -4,7 +4,7 @@ __author__ = 'tang'
 from crawl import Getter
 from bs4 import BeautifulSoup
 
-t = Getter("NHS.data")
-for c in range(65, 91):
-    t.traverse_alphabet_index(chr(c))
-t.save()
+t = Getter("NHS.data", read=True)
+
+
+t.to_database_format(open("NHS_DB.data", "w"))
