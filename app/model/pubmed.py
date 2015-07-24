883a9348d0aa109c510696431eca60305acc9349
unknown <tanghaomo@126.com>
2015-07-25 04:14:06 +0800

    Bug fixed

--- app/model/pubmed.py
+++ app/model/pubmed.py
@@ -1,32 +1,34 @@
 __author__ = 'zhou'
 
-import tempfile, urllib2, json
+import urllib2
+import json
 from bs4 import BeautifulSoup
 
+
 class PubmedGetter:
-	def __init__(self, keyword):
-		self.keyword = keyword.replace(" ", "+")
-		self.url = "http://www.ncbi.nlm.nih.gov/pubmed/?term=" + self.keyword
-		self.synonym = None
-		self.textarea = None
+    def __init__(self, keyword):
+        self.keyword = keyword.replace(" ", "+")
+        self.url = "http://www.ncbi.nlm.nih.gov/pubmed/?term=" + self.keyword
+        self.synonym = None
+        self.textarea = None
 
-	def send(self):
-		try:
-			page = urllib2.urlopen(self.url)
-		except urllib2.URLError:
-			print("Cannot open page with keyword:" + self.keyword)
-			return False
-		soup = BeautifulSoup(page, 'html.parser')
-		self.textarea = str(soup.find_all("textarea", class_="searchdetails_term")[0])
+    def send(self):
+        try:
+            page = urllib2.urlopen(self.url)
+        except urllib2.URLError:
+            print("Cannot open page with keyword:" + self.keyword)
+            return False
+        soup = BeautifulSoup(page, 'html.parser')
+        self.textarea = str(soup.find_all("textarea", class_="searchdetails_term")[0])
 
-	def extract(self):
-		#text = self.textarea[0][self.textarea[0].index('>') + 1 : self.textarea[0].index('<') - 1]
-		#self.textarea = str(self.textarea[0])
-		text = self.textarea[self.textarea.index('>') + 1 : self.textarea.index('<') - 1]
-		chopping = text.find('[MeSH Terms');
-		if chopping < 0:
-			return "[]"
-		text = text[0 : text.find('[MeSH Terms]')]
-		text = text[1 : text.index('"', 1)]
+    def extract(self):
+        #text = self.textarea[0][self.textarea[0].index('>') + 1 : self.textarea[0].index('<') - 1]
+        #self.textarea = str(self.textarea[0])
+        text = self.textarea[self.textarea.index('>') + 1 : self.textarea.index('<') - 1]
+        chopping = text.find('[MeSH Terms')
+        if chopping < 0:
+            return "[]"
+        text = text[0 : text.find('[MeSH Terms]')]
+        text = text[1 : text.index('"', 1)]
 
-		return json.dumps(text.split(','))
+        return json.dumps(text.split(','))
