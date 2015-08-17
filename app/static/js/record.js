7f9d61ed0ab7cf8958595df386ef67fefbfad765
Zhou Wei <zhouwei1026@gmail.com>
2015-08-17 08:38:51 -0400

    Making record function done!

--- app/static/js/record.js
+++ app/static/js/record.js
@@ -1,5 +1,6 @@
 var diseaseId;
 var diseaseName;
+var recordId = 0;
 
 function getRecord(diseaseId){
 	if(!loginInfo.receive){
@@ -26,15 +27,25 @@ function getRecord(diseaseId){
 
 function getRecordCallback(diseaseId){
 	return function(data){
+		console.log(data);
 		for(var index in data){
 			if(data[index]['diseases'][0]['id'] == diseaseId){
 				console.log(diseaseId + ' matched');
+				loadRecordData(data[index]);
 				$('#collected').show();
 				return;
 			}
 		}
 		$('#collect').show();
 	}
+}
+
+function loadRecordData(data){
+	recordId = data['id'];
+	$('#collected-comment').val(data['comment']);
+	$('#collected-satisfying').val(data['satisfying']);
+	$('#collected-satisfyingValue').text(data['satisfying']);
+
 }
 
 function addRecord(diseaseId, comment, satisfying){
@@ -56,12 +67,57 @@ function addRecord(diseaseId, comment, satisfying){
 	});
 }
 
+function modifyRecord(comment, satisfying){
+	$.ajax({
+		url: '/api/record',
+		type: 'PUT',
+		dataType: 'json',
+		contentType: 'application/json',
+		data: JSON.stringify({record_id:recordId, comment: comment, satisfying: satisfying}),
+		success: function(data){
+			console.log(data);
+			window.location.reload();
+		},
+		error: function(){
+			console.log('Server is busy');
+		}
+	});
+}
+
+function deleteRecord(){
+	if(!confirm('Are you sure you want to remove this disease from your collection list?')){
+		return;
+	}
+	if(recordId == 0){
+		alert('Error Occurs when removing this record. Please try later');
+		return;
+	}
+	$.ajax({
+		url: '/api/record/' + recordId,
+		type: 'DELETE',
+		dataType: 'json',
+		success:function(data){
+			if(typeof data['Err'] !== 'undefined' && data['Err'] != ''){
+				alert(data['Err']);
+			}
+			window.location.reload();
+		},
+		error: function(){
+			console.log('Server is busy');
+		}
+	});
+}
+
 $(document).ready(function(){
 	diseaseId = window.location.pathname.split('/')[window.location.pathname.split('/').length - 1];
 	getRecord(diseaseId);
 	$('#record-add-form').submit(function(event){
 		event.preventDefault();
 		addRecord(diseaseId, $('#record-add-comment').val(), $('#record-add-satisfying').val());
+	});
+	$('#collected-form').submit(function(event){
+		event.preventDefault();
+		modifyRecord($('#collected-comment').val(), $('#collected-satisfying').val());
 	});
 });
 
