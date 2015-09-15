abbb3cdd758fb81f998862b6e4dd032223c6063d
Alston Tang <tanghaomo@126.com>
2015-09-16 02:55:26 +0800

    improve documentation

--- app/views.py
+++ app/views.py
@@ -1,13 +1,57 @@
-from time import time
-import json
 import copy
 
-from flask import render_template, request, send_from_directory, session, url_for
+from datetime import timedelta
+from functools import update_wrapper
+from flask import render_template, request, send_from_directory, session, url_for, current_app, make_response
 from sqlalchemy import distinct, exc, or_
 
 from app import app
 from model import *
 from helper import *
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    # Decorator for adding cross origin header
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = current_app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = current_app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+
 
 
 @app.route('/')
@@ -66,6 +110,7 @@ def confirm_password(jwt_data):
 
 
 @app.route('/doc/<file_name>')
+@crossdomain('*')
 def document(file_name):
     import os
     path = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "doc")
